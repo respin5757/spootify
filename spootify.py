36f65f1fd452f2e06cac9b655e85321601104739
Rafael Espinoza <78329514+respin5757@users.noreply.github.com>
2022-11-28 19:51:36 -0500

    Update spootify.py
    get tracks, track ids, then retrieve features from each id and take note of them. Average them out to help a fundamental mood-guessing function. Also added a function to plot the features to a spider chart.

--- spootify.py
+++ spootify.py
@@ -5,42 +5,107 @@ import requests
 from requests_oauthlib import OAuth2Session
 from requests.auth import HTTPBasicAuth
 import json
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 #app credentials
 client_id = cred.CLIENT_ID
 client_secret = cred.CLIENT_SECRET
 redirect_uri = cred.REDIRECT_URL
 
-# OAuth endpoints given in the Spotify API documentation
-# https://developer.spotify.com/documentation/general/guides/authorization/code-flow/
-authorization_base_url = "https://accounts.spotify.com/authorize"
-token_url = "https://accounts.spotify.com/api/token"
-# https://developer.spotify.com/documentation/general/guides/authorization/scopes/
-scope = ["user-read-email","playlist-read-collaborative" ]
+def authorization():
+    authorization_base_url = "https://accounts.spotify.com/authorize"
+    token_url = "https://accounts.spotify.com/api/token"
+    scope = ["user-read-email","playlist-read-collaborative", "user-read-recently-played" ]
 
 
-spotify = OAuth2Session(client_id, scope=scope, redirect_uri=redirect_uri)
+    spotify = OAuth2Session(client_id, scope=scope, redirect_uri=redirect_uri)
 
-# Redirect user to Spotify for authorization
-authorization_url, state = spotify.authorization_url(authorization_base_url)
+    # Redirect user to Spotify for authorization
+    authorization_url, state = spotify.authorization_url(authorization_base_url)
 
-print('Please go here and authorize: ', authorization_url)
+    print('Please go here and authorize: ', authorization_url)
 
-# Get the authorization verifier code from the callback url
-redirect_response = input('\n\nPaste the full redirect URL here: ')
+    # Get the authorization verifier code from the callback url
+    redirect_response = input('\n\nPaste the full redirect URL here: ')
+
+    auth = HTTPBasicAuth(client_id, client_secret)
+
+    # Fetch the access token
+    token = spotify.fetch_token(token_url, auth=auth, authorization_response=redirect_response)
+
+    #fetch recently played
+    x = spotify.get('https://api.spotify.com/v1/me/player/recently-played')
+
+    
+    #getting ids from recently played
+    x = x.json()
+    tracks =x['items']
+    track_ids= []
+    tracks_names= []
+    for item in tracks:
+        track_ids.append(item['track']['id'])
+        tracks_names.append(item['track']['name'])
 
 
+    #getting features from track id and adding
+    valence, danceability, energy, liveness= 0, 0, 0, 0
+    for ids in track_ids:
+        link= "https://api.spotify.com/v1/audio-features/"+ids
+        song_feature= spotify.get(link)
+        song_feature = song_feature.json()
+        valence+= song_feature["valence"]
+        danceability+= song_feature["danceability"]
+        energy+= song_feature["energy"]
+        liveness+= song_feature["liveness"]
 
-auth = HTTPBasicAuth(client_id, client_secret)
+    #finding averages    
+    valence= valence/ len(track_ids)
+    danceability= danceability/ len(track_ids)
+    energy= energy/ len(track_ids)
+    liveness= liveness/ len(track_ids)
+    stats= [valence, danceability, energy, liveness]
+    
+    return stats, tracks_names
+    #print("valence: ", valence, "\ndanceability: ", danceability, "\nenergy: ", energy, "\nliveness: ", liveness)   
 
-# Fetch the access token
-token = spotify.fetch_token(token_url, auth=auth, authorization_response=redirect_response)
+def mood_finder(stats):
+    if (stats[0]>0.5) & (stats[2]>0.5):
+        return "happy"
+    elif (stats[0]<0.334) & (stats[2]>0.333) & (stats[1]>0.333) & (stats[3]<0.666):
+        return "angsty"
+    else:
+        return "sad"
+    
+def plot_stats(stats):     
+    #plotting using matplotlib
+    categories= ['Valence', 'Danceability','Energy','Liveness']
+    categories = [*categories, categories[0]]
+    stats = [*stats, stats[0]]
+    label_loc = np.linspace(start=0, stop=2 * np.pi, num=len(stats))
+    plt.figure(figsize=(8, 8))
+    plt.subplot(polar=True)
+    plt.plot(label_loc, stats)
+    plt.title('Recently-Played Song Features', size=15)
+    lines, labels = plt.thetagrids(np.degrees(label_loc), labels=categories)
+    plt.legend()
+    plt.show()
 
-#print(token)
+def main():
+    
+    stats, tracks_names= authorization() 
+    print("\nBased on your 20 most recently-played songs, we found that you are most likely feeling",mood_finder(stats),".")
+    val= input("\nWould you like to see your song features' averages? <yes/no> ")
+    if val=="yes":
+        #print("songs: ")
+        #for i in tracks_names:
+        #    print(i, end= ', ')
+        plot_stats(stats)
+    elif val=="no":
+        print("thank you for trying out my web application!")
+    else:
+        print("only valid inputs are 'yes' or 'no'")
 
-# Fetch a protected resource, i.e. user profile
-r = spotify.get('https://api.spotify.com/v1/me')
-#print(r.content)
-y = r.json()
-print("Username: ", y["display_name"])
 
+main()
